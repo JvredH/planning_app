722cc1ee783254c78d409914536ffd69502a4d5a
jared <hem.jared@gmail.com>
2023-08-09 10:18:37 -0700

    pseduo for routes

--- routes/planning.js
+++ routes/planning.js
@@ -13,3 +13,32 @@ router.post('/planner', (req, res) => {
 });
 
 // pseudo code the rest of the routes
+
+router.get('plans', (req, res) => {
+  // send query to database to get previously created plans for current user
+  // send back the plans
+})
+
+router.post('/plans', (req, res) => {
+  // parse req body
+  // add to database
+  // send success response
+})
+
+router.delete('/plans', (req, res) => {
+  // take id from params
+  // delete entry with same id from db
+  // send success response
+})
+
+router.get('/history', (req, res) => {
+  // take id from params
+  // query db to get data,
+  // send data back to user
+})
+
+router.delete('/history', (req, res) => {
+  // take id from params
+  // query db to delete the entry
+  // send success response
+})
